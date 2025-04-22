e6d02ea1013c985a63bf549370a8e075af9f2a42
Mingjian Li <131323777+Normanisfine@users.noreply.github.com>
2025-04-21 21:04:36 -0400

    config reverse

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,7 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: 'export', // ✅ enables static export
-  };
-  
-  export default nextConfig;
-  
+const nextConfig = {};
+
+export default nextConfig;
